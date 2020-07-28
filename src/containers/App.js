19c9935a21a88e50249f44c367c3328ca37e71f6
Jordan Walker <jwalker117@gmail.com>
2020-07-28 10:48:32 -0500

    reconfigured navigation and app in order to do the layout correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,16 +3,28 @@ import React from 'react';
 //Import Components
 import Navigation from '../components/Navigation/Navigation';
 
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+//Import pages 
+import Home from '../components/pages/Home/Home';
+import Menu from '../components/pages/Menu/Menu';
+import About from '../components/pages/About/About';
+
 //Needed to activate Bulma on the project
 //After npm install
 import 'bulma/css/bulma.css';
 
 function App() {
   return (
-    <div class="container">
-      <Navigation />
-      
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Route exact path='/' component={Home}/>
+        <Route path='/menu' component={Menu}/>
+        <Route path='/about' component={About}/>
+
+      </div>
+    </Router>
   );
 }
 
