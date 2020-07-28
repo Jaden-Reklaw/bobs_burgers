76775d2933dacb68825e097d498231ed3837da57
Jordan Walker <jwalker117@gmail.com>
2020-07-28 14:30:25 -0500

    added favicon to head

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,14 +4,13 @@ import { Link } from 'react-router-dom';
 class Navigation extends Component {
   render() {
     return (
-        
         <nav class="navbar" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28" />
+                <a class="navbar-item" href="/">
+                    <img src="./images/BB_logo.png" width="112" height="28" />
                 </a>
 
-                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
@@ -20,22 +19,32 @@ class Navigation extends Component {
 
             <div id="navbarBasicExample" class="navbar-menu">
                 <div class="navbar-start">
-                    <a class="navbar-item">
-                        <Link to='/'>Home</Link>
+                <a class="navbar-item">
+                    <Link to='/'>Home</Link>
+                </a>
+
+                <a class="navbar-item">
+                    <Link to='/menu'>Menu</Link>
+                </a>
+
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                    More
                     </a>
 
+                    <div class="navbar-dropdown">
                     <a class="navbar-item">
-                        <Link to='/menu'>Menu</Link>
+                    <Link to='/about'>About</Link>
                     </a>
-
                     <a class="navbar-item">
-                        <Link to='/about'>About</Link>
+                        Contact
                     </a>
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link">
-                            More
-                        </a>
+                    <hr class="navbar-divider" />
+                    <a class="navbar-item">
+                        Report an issue
+                    </a>
                     </div>
+                </div>
                 </div>
             </div>
         </nav>
