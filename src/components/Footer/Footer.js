66e2ce9f526afa277f60681eb36458da9c17922a
Jordan Walker <jwalker117@gmail.com>
2020-08-03 17:54:51 -0500

    add social links to sub footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,7 @@ class Footer extends Component {
     return (
       <footer>
         <div className="container is-fullhd">
+            {/* Main Footer */}
             <div className="columns">
                 <div className="column">
                     <h3>About Us</h3>
@@ -26,7 +27,17 @@ class Footer extends Component {
                     <h3>Contact US</h3>
                 </div>
             </div>
+            {/* Sub Footer Social Links */}
+            <i className="fa fa-github fa-3x" aria-hidden="true"></i>
+            <i className="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
+            <i className="fa fa-twitter-square fa-3x" aria-hidden="true"></i>
+            <i className="fa fa-youtube-play fa-3x" aria-hidden="true"></i>
+            <i className="fa fa-instagram fa-3x" aria-hidden="true"></i>
+            <i className="fa fa-tumblr-square fa-3x" aria-hidden="true"></i>
+            <i className="fa fa-spotifyfa-3x" aria-hidden="true"></i>
             <hr />
+
+            
             
         </div>
       </footer>
